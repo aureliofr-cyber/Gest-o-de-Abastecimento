fced70a12aebbb9f943426008aa8948ed52783c4
aureliofr-cyber <aurelio.fr@hotmail.com>
2026-06-23 01:02:39 -0300

    Ajustes e correções
    inserção de novas ferramentas

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "hodometro-v13";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE = "hodometro-v20";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
