6f49984dd0d8ad8ade194d3da08e7da49d683db8
aureliofr-cyber <aurelio.fr@hotmail.com>
2026-06-23 01:33:59 -0300

    ajustes
    ajustes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hodometro-v20";
+const CACHE = "hodometro-v21";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 self.addEventListener("install", (e) => {
