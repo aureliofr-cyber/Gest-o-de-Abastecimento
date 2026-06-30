4adea2e6c99d597124fb89e134241df88aa4e1ab
aureliofr-cyber <aurelio.fr@hotmail.com>
2026-06-30 09:15:57 -0300

    Atualizações
    Atualizações

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hodometro-v39";
+const CACHE = "hodometro-v53";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 self.addEventListener("install", (e) => {
